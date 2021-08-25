05bdf52e6e60451d75d101ea64068f5c02494f8b
Mohammad Amin Zare Soltani <48747618+mazsoltani@users.noreply.github.com>
2021-08-25 06:43:46 +0430

    Add files via upload

---
+++ lozi.py
@@ -0,0 +1,13 @@
+n=int(input('Please Enter a Number= '))
+
+for i in range(n*2-1):
+    for j in range(n*2-1):
+        if (i<n-1) and ((j+i < n-1) or (j-i > n-1)):
+            print(' ',end = '')
+        elif (i>n-1) and ((i-j>n-1) or (i+j>(2*n)+(n-3))):
+            print(' ',end = '')
+        else:    
+            print('*',end='')
+    print()
+    
+print('Program is Finished')
